85a66363d6eebaa4f8bd2bf008a0d2c56a4b4cb4
Sushmita Paul <sushmitapaul@Sushmitas-iMac.local>
2012-08-11 19:04:09 +0530

    Initial Commit

---
+++ testC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testC
+//
+//  Created by Sushmita Paul on 11/08/12.
+//  Copyright (c) 2012 Sushmita Paul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
